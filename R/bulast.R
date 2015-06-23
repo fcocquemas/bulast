ffe80a37606b0a832c37377c4b62353655aab02a
Francois Cocquemas <francois.cocquemas@gmail.com>
2015-06-23 12:30:57 -0500

    Rewrote with data.table as a dependency and faster rbinding with rbindlist.

--- R/bulast.R
+++ R/bulast.R
@@ -13,8 +13,9 @@
 #' @param annualaverage boolean include annual average ("M13") data; requires
 #'   registrationKey
 #'   
-#' @return list with names status (character), responseTime (integer), message
-#'   (list of character), Results (data.table)
+#' @return Returns a list with names status (character), responseTime (integer), message
+#'   (list of character), Results (data.table). Results has a date field that marks the
+#'   end of the month.
 #' 
 #' @examples
 #' r <- bulast(c('LAUCN040010000000005', 'LAUCN040010000000006'))
@@ -26,53 +27,68 @@ bulast <- function (seriesid, startyear = NULL, endyear = NULL, registrationKey 
 {
   # Build payload with non-NULL arguments only
   payload <- Filter(is.character, sapply(c("startyear", "endyear", "registrationKey", 
-                      "catalog", "calculations", "annualaverage"), function(x) {
-                        if(exists(x) & eval(parse(text=paste0("!is.null(", x, ")")))) {
-                          tolower(as.character(eval(parse(text=x))))
-                        }
-                      }))
+                                           "catalog", "calculations", "annualaverage"), function(x) {
+                                             if(exists(x) & eval(parse(text=paste0("!is.null(", x, ")")))) {
+                                               tolower(as.character(eval(parse(text=x))))
+                                             }
+                                           }))
   payload[["seriesid"]] <- seriesid
   
   # Pick V2 url if registrationKey is present, V1 otherwise
   url <- ifelse("registrationKey" %in% names(payload),
-    "http://api.bls.gov/publicAPI/v2/timeseries/data/",
-    "http://api.bls.gov/publicAPI/v1/timeseries/data/")
+                "http://api.bls.gov/publicAPI/v2/timeseries/data/",
+                "http://api.bls.gov/publicAPI/v1/timeseries/data/")
   
   # Perform request
   r <- httr::content(httr::POST(url, body = rjson::toJSON(payload), 
                                 httr::content_type_json()))
   
-  # Put results into data.table format
-  r$Results <- do.call("rbind", lapply(r$Results$series, function(s) {
-    df <- do.call("rbind", lapply(s$data, function(d) {
-      df <- data.table(year = as.integer(d[["year"]]), 
-                       period = d[["period"]], periodName = d[["periodName"]], 
-                       value = as.numeric(d[["value"]]), 
-                       footnotes = paste(unlist(d[["footnotes"]]), collapse = " "))
+  # Check that we actually have data
+  if(length(r$Results) > 0) {
+    # Put results into data.table format
+    dt <- data.table::rbindlist(lapply(r$Results$series, function(s) {
+      dt <- data.table::rbindlist(lapply(s$data, function(d) {
+        d[["value"]] <- as.numeric(d[["value"]])
+        d[["footnotes"]] <- paste(unlist(d[["footnotes"]]), collapse = " ")
+        if ("calculations" %in% names(d)) {
+          d[["pct_ch_1"]] <- as.numeric(d[["calculations"]]$pct_changes$`1`)
+          d[["pct_ch_3"]] <- as.numeric(d[["calculations"]]$pct_changes$`3`)
+          d[["pct_ch_6"]] <- as.numeric(d[["calculations"]]$pct_changes$`6`)
+          d[["pct_ch_12"]] <- as.numeric(d[["calculations"]]$pct_changes$`12`)
+          d[["calculations"]] <- NULL
+        }
+        d <- lapply(lapply(d, unlist), paste, collapse=" ")
+        
+        d
+      }), use.names = TRUE, fill=TRUE)
       
-      if ("calculations" %in% names(d)) {
-        df$pct_ch_1 <- as.numeric(d[["calculations"]]$pct_changes$`1`)
-        df$pct_ch_3 <- as.numeric(d[["calculations"]]$pct_changes$`3`)
-        df$pct_ch_6 <- as.numeric(d[["calculations"]]$pct_changes$`6`)
-        df$pct_ch_12 <- as.numeric(d[["calculations"]]$pct_changes$`12`)
-      }
-
-      df
-    }), fill=TRUE)
-    df$seriesID <- s[["seriesID"]]
-    df
-  }), fill=TRUE)
+      dt[, seriesID := s[["seriesID"]]]
+      dt
+    }), use.names = TRUE, fill=TRUE)
   
-  # Add a date field
-  r$Results[, date := 
-    as.Date(paste(year, ifelse(period == "M13", 12, substr(period, 2, 3)), "01", sep = "-")) + months(1) - days(1),
-    by="year,period"]
+    # Add a date field
+    dt[, date := seq(as.Date(paste(year, ifelse(period == "M13", 12, substr(period, 2, 3)), "01", sep = "-")),
+                     length = 2, by = "months")[2]-1,
+       by="year,period"]
+    
+    # Remove year and period fields
+    dt[, `:=`(year = NULL, period = NULL)]
+    
+    # Remove periodName but add periodType (monthly or annual)
+    dt[, `:=`(periodType = factor(ifelse(periodName == "Annual", "Annual", "Monthly")), periodName = NULL)]
+    
+    # Reorder columns
+    setcolorder(dt, c("seriesID", "date", "value", "periodType",
+                      colnames(dt)[!colnames(dt) %in% 
+                                     c("seriesID", "date", "value", "periodType")]))
+    
+    # Key by series, data
+    setkey(dt, "seriesID", "date")
+    
+    # Replace Results with data.table
+    r$Results <- dt
+  }
   
-  # Remove year and period fields
-  r$Results[, `:=`(year = NULL, period = NULL)]
-
-  # Key by series, data
-  setkey(r$Results, "seriesID", "date")
   
   r
 }
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # bulast - A simpler R package to access the Bureau of Labor Statistics (BLS) API
 
-This package provides access to the Bureau of Labor Statistics API in an (arguably) friendlier interface than the blsAPI package, by hiding the JSON code under the hood. 
+This package provides access to the Bureau of Labor Statistics API in an (arguably) friendlier interface than the blsAPI package, by hiding the JSON code under the hood and being a little more opinionated. It might not suit your use case, in which case you should definitely check out blsAPI.
 
-It relies on `httr` and returns the results as a data.table with a Date field set at the last day of the period.
+It relies on `httr`, and returns the results as a data.table with a `date` field set at the last day of the period. That would be December 31 for both December and annual data, so a periodType field is set to either "Monthly" or "Annual".
+
+Not yet tested with quarterly or semiannual data, and it will likely choke on them.
 
 # Installation
 
-For now it is not on CRAN since it is likely to evolve. You will have to use the `devtools` package to install directly from Github:
+For now, it has not been submitted to CRAN since it is likely to evolve. You will have to use the `devtools` package to install directly from Github:
 
 ```{r}
 devtools::install_github("fcocquemas/bulast")
@@ -30,25 +32,26 @@ $status
 [1] "REQUEST_SUCCEEDED"
 
 $responseTime
-[1] 629
+[1] 89
 
 $message
 list()
 
 $Results
-    year period periodName value footnotes             seriesID
- 1: 2012    M12   December 18281           LAUCN040010000000005
- 2: 2012    M11   November 18242           LAUCN040010000000005
- 3: 2012    M10    October 18619           LAUCN040010000000005
- 4: 2012    M09  September 19008           LAUCN040010000000005
+                seriesID       date value periodType footnotes
+ 1: LAUCN040010000000005 2010-01-31 19164    Monthly          
+ 2: LAUCN040010000000005 2010-02-28 18996    Monthly          
+ 3: LAUCN040010000000005 2010-03-31 19252    Monthly          
+ 4: LAUCN040010000000005 2010-04-30 19834    Monthly          
 
 ...
 
-69: 2010    M04      April 23569           LAUCN040010000000006
-70: 2010    M03      March 23209           LAUCN040010000000006
-71: 2010    M02   February 23156           LAUCN040010000000006
-72: 2010    M01    January 23374           LAUCN040010000000006
-    year period periodName value footnotes             seriesID
+69: LAUCN040010000000006 2012-09-30 23249    Monthly          
+70: LAUCN040010000000006 2012-10-31 22888    Monthly          
+71: LAUCN040010000000006 2012-11-30 22258    Monthly          
+72: LAUCN040010000000006 2012-12-31 22415    Monthly          
+                seriesID       date value periodType footnotes
+
 ```
 
 
@@ -57,23 +60,18 @@ A more complex example with the V2 API (requires a valid [registration key from 
 ```{r}
 r <- bulast(c('LAUCN040010000000005', 'LAUCN040010000000006'),
             startyear = 2010, endyear = 2012,
-            registrationKey = "995f4e779f204473aa565256e8afe73e", calculations = TRUE)
-r$Results
+            registrationKey = "cfbcf439326c4a2faa70451d74013819", 
+            calculations = TRUE, annualaverage = TRUE)
+r$Results[periodType=="Annual"]
 ```
 
 This returns:
 ```
-    year period periodName value footnotes pct_ch_1 pct_ch_3 pct_ch_6 pct_ch_12             seriesID
- 1: 2012    M12   December 18281                0.2     -3.8     -2.0      -2.3 LAUCN040010000000005
- 2: 2012    M11   November 18242               -2.0     -3.9     -1.1      -2.2 LAUCN040010000000005
- 3: 2012    M10    October 18619               -2.0     -1.5      2.1      -0.7 LAUCN040010000000005
- 4: 2012    M09  September 19008                0.2      1.9      4.1       0.5 LAUCN040010000000005
-
-...
-
-69: 2010    M04      April 23569                1.6      0.8      1.9       5.5 LAUCN040010000000006
-70: 2010    M03      March 23209                0.2      1.3     -1.1       5.9 LAUCN040010000000006
-71: 2010    M02   February 23156               -0.9      1.1     -5.1       5.2 LAUCN040010000000006
-72: 2010    M01    January 23374                2.0      1.0     -3.7       6.3 LAUCN040010000000006
-    year period periodName value footnotes pct_ch_1 pct_ch_3 pct_ch_6 pct_ch_12             seriesID
+               seriesID       date value periodType footnotes pct_ch_1 pct_ch_3 pct_ch_6 pct_ch_12
+1: LAUCN040010000000005 2010-12-31 19589     Annual                2.2      0.7     -1.5       1.4
+2: LAUCN040010000000005 2011-12-31 18794     Annual                0.4      0.3        0      -4.1
+3: LAUCN040010000000005 2012-12-31 18499     Annual                1.2     -0.6     -2.1      -1.6
+4: LAUCN040010000000006 2010-12-31 23438     Annual                3.3      1.8     -2.6       2.2
+5: LAUCN040010000000006 2011-12-31 23065     Annual                0.3     -0.3     -2.6      -1.6
+6: LAUCN040010000000006 2012-12-31 22878     Annual                2.1        0     -4.5      -0.8
 ```
